24afce262fbc8dbcc007f8f0cbbe9db037c6378e
abhib <abhib@nvidia.com>
2024-03-24 10:10:18 -0700

    changes to add prompts

--- frontend/app.py
+++ frontend/app.py
@@ -1,9 +1,13 @@
 import streamlit as st
 import random
+import json
 from psychometry import psychometry_page
 from simple_chat import simple_chat
 from multimodal_chat import multimodal_chat
 from journal import journal_app
+
+with open("C:\\Apps\\nd-empowerment\\utils\\style_guide.json", "r") as file:
+    json_data = json.load(file)
 
 def main():
     st.sidebar.title('Navigation')
@@ -12,7 +16,7 @@ def main():
     if page == 'Psychometry':
         psychometry_page()
     elif page == 'Chat':
-        simple_chat(f"Mocked response from assistant. {random.randint(1, 100)}")
+        simple_chat(json_data)
     elif page == "Multimodal Chat":
         multimodal_chat()
     elif page == "Journal":
--- frontend/simple_chat.py
+++ frontend/simple_chat.py
@@ -2,33 +2,45 @@ import streamlit as st
 from litellm import completion
 import dotenv
 dotenv.load_dotenv('../utils/.env')
+# import os
+# os.getenv('MISTRAL_API_KEY')
 
 
 # client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
-def completion_llm(text,model='mistral/mistral-small-latest'):
-    messages = [{ "content": text,"role": "user"}]
+def completion_llm(text, model='mistral/mistral-small-latest'):
+    messages = [{ "content": text, "role": "user"}]
     response = completion(model=model, messages=messages)
     return response.choices[0].message.content
 
-def chat_llm(input, model='mistral/mistral-small-latest'):
-    messages = [{ "content": input,"role": "user"}]
+
+def chat_llm(input_messages, model='mistral/mistral-small-latest'):
+    messages = input_messages
     response = completion(model=model, messages=messages)
     # append response as a new message with role 'assistant'
-    messages.append({ "content": response.choices[0].message.content, "role": "assistant"})
-    return messages
+    return response.choices[0].message.content
 
 
-def simple_chat(mock_response=None):
+def simple_chat(style_guide):
 
-    if "openai_model" not in st.session_state:
-        st.session_state["openai_model"] = "gpt-3.5-turbo"
+    # if "openai_model" not in st.session_state:
+    #     st.session_state["openai_model"] = "gpt-3.5-turbo"
 
     if "messages" not in st.session_state:
         st.session_state.messages = []
 
     for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
+        if message['role'] != 'system':
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
+
+    system_prompt = f"""
+        You are a helpful assistant that response to user query using the defined style guide.
+        Refrain from adding any external information and abide to the Style Guide and its structure.
+
+        Style Guide : {style_guide}
+        """
+
+    st.session_state.messages.append({"role": "system", "content": system_prompt})
 
     if prompt := st.chat_input("What is up?"):
         st.session_state.messages.append({"role": "user", "content": prompt})
@@ -36,9 +48,15 @@ def simple_chat(mock_response=None):
             st.markdown(prompt)
 
         with st.chat_message("assistant"):
-            if mock_response:
-                response = mock_response
-                st.markdown(mock_response)
+            messages = [
+                        {"role": m["role"], "content": m["content"]}
+                        for m in st.session_state.messages
+                    ]
+            response = chat_llm(messages)
+            st.markdown(response)
+            # if mock_response:
+            #     response = mock_response
+            #     st.markdown(mock_response)
             # else:
             #     stream = client.chat.completions.create(
             #         model=st.session_state["openai_model"],
